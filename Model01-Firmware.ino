d9f8ffe6fc06c2f260c09367217246eff1add54f
Hugo Forte <hforte@gcmlp.com>
2020-03-15 08:02:39 -0500

    switched fn buttons with ctrl and shift

--- Model01-Firmware.ino
+++ Model01-Firmware.ino
@@ -229,15 +229,15 @@ KEYMAPS(
    Key_Backtick, Key_Q, Key_W, Key_E, Key_R, Key_T, Key_Tab,
    Key_Home,   Key_A, Key_S, Key_D, Key_F, Key_G,
    Key_End, Key_Z, Key_X, Key_C, Key_V, Key_B, Key_Escape,
-   Key_LeftControl, Key_Backspace, Key_LeftShift, Key_LeftGui,
-   ShiftToLayer(FUNCTION),
+   ShiftToLayer(FUNCTION), Key_Backspace, Key_LeftShift, Key_LeftGui,
+   Key_LeftControl,
 
    M(MACRO_ANY),  Key_6, Key_7, Key_8,     Key_9,         Key_0,         LockLayer(NUMPAD),
    Key_Enter,     Key_Y, Key_U, Key_I,     Key_O,         Key_P,         Key_Equals,
                   Key_H, Key_J, Key_K,     Key_L,         Key_Semicolon, Key_Quote,
    LCTRL(LALT(Key_Delete)),  Key_N, Key_M, Key_Comma, Key_Period,    Key_Slash,     Key_Minus,
-   Key_RightShift, Key_LeftAlt, Key_Spacebar, Key_RightShift,
-   ShiftToLayer(SELECTS)),
+   Key_RightShift, Key_LeftAlt, Key_Spacebar, ShiftToLayer(SELECTS),
+   Key_RightShift),
 
 #else
 
