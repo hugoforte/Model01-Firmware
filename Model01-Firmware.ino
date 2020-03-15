2d4bf75c7a1db4882aa59807cfe7dd9ee217c145
Hugo Forte <hforte@gcmlp.com>
2020-03-15 08:30:15 -0500

    Added more navigation - select up and down

--- Model01-Firmware.ino
+++ Model01-Firmware.ino
@@ -286,7 +286,7 @@ KEYMAPS(
    ___,
 
    Consumer_ScanPreviousTrack, Key_F6,                        Key_F7,                   Key_F8,                   Key_F9,          Key_F10,          Key_F11,
-   Consumer_PlaySlashPause,    Consumer_ScanNextTrack,        LCTRL(Key_LeftArrow),     LCTRL(Key_RightArrow),    Key_LeftBracket, Key_RightBracket, Key_F12,
+   Consumer_PlaySlashPause,    LSHIFT(Key_DownArrow),        LCTRL(Key_LeftArrow),     LCTRL(Key_RightArrow),    LSHIFT(Key_UpArrow), Key_RightBracket, Key_F12,
                                LCTRL(LSHIFT(Key_LeftArrow)),  LSHIFT(Key_LeftArrow),    LSHIFT(Key_RightArrow),  LCTRL(LSHIFT(Key_RightArrow)),       ___,              ___,
    Key_PcApplication,          Consumer_Mute,                 Consumer_VolumeDecrement, Consumer_VolumeIncrement, ___,             Key_Backslash,    Key_Pipe,
    ___, ___, Key_Enter, ___,
